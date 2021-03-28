62d553d88bb2653481a948385bbc8978abd38635
marshall4471 <72532709+marshall4471@users.noreply.github.com>
2021-03-28 11:29:08 -0500

    Update python_or_anaconda.py

--- python_or_anaconda.py
+++ python_or_anaconda.py
@@ -39,9 +39,9 @@ test_set = test_datagen.flow_from_directory(file2,
                                             batch_size = 16,
                                             class_mode = 'binary')
 model = tf.keras.models.Sequential()
-model.add(tf.keras.layers.Conv2D(128, kernel_size=5, padding='valid', activation='relu', input_shape=[384, 384, 3]))
+model.add(tf.keras.layers.Conv2D(128, kernel_size=[5,5], padding='valid', activation='relu', input_shape=[384, 384, 3]))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=[3,3], strides=2, padding='valid'))
-model.add(tf.keras.layers.Conv2D(32, kernel_size=3, padding='valid', activation='relu'))
+model.add(tf.keras.layers.Conv2D(32, kernel_size=[3,3], padding='valid', activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=[3,3], strides=2, padding='valid'))
 model.add(tf.keras.layers.Flatten())
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
@@ -50,7 +50,7 @@ model.summary()
 
 history = model.fit(train_set, validation_data =test_set, epochs=15, verbose=1)
 history.history['accuracy']
-model.save("snake_pred5.h5")
+model.save("snake_pred6.h5")
 
 
 
